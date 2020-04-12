362d9d80854d166d8ae31e999f28d8787c550d6d
Valeriya Tarasova <vt230319@gmail.com>
2020-04-13 01:44:30 +0300

    added days 4 and 5

--- Day4/src/Sample1.java
+++ Day4/src/Sample1.java
@@ -1,5 +1,3 @@
-
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -26,4 +24,3 @@ public class Sample1 {
         safariDriver.close();
     }
 }
-
